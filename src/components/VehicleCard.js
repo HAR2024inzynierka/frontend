163108631fb258d30c4544660e206035483d1e2b
Artsiom <gwifen228322@gmail.com>
2024-11-17 00:37:24 +0100

    Most of frontend ready - Hania

--- src/components/VehicleCard.js
+++ src/components/VehicleCard.js
@@ -1,6 +1,6 @@
-// components/VehicleCard.js
 import React from 'react';
 import styled from 'styled-components';
+
 
 const Card = styled.div`
     background: white;
@@ -10,7 +10,7 @@ const Card = styled.div`
     padding: 20px;
     width: 250px;
     transition: transform 0.2s;
-    cursor: pointer; /* Dodanie wskaźnika, że karta jest klikalna */
+    cursor: pointer;
 
     &:hover {
         transform: scale(1.05);
@@ -30,11 +30,11 @@ const Info = styled.p`
 const Button = styled.button`
     margin: 5px 5px;
     color: #01295F;
-    border: 1px solid #01295F; /* Dodajemy obramowanie */
+    border: 1px solid #01295F;
     background: white;
-    width: 100px; /* Ustalamy stałą szerokość */
-    height: 40px; /* Ustalamy stałą wysokość */
-    border-radius: 5px; /* Zaokrąglone rogi */
+    width: 100px;
+    height: 40px;
+    border-radius: 5px;
 
     &:hover {
         background: #01295F;
@@ -42,29 +42,27 @@ const Button = styled.button`
     }
 `;
 
-const ButtonDelete = styled(Button)` /* Dziedziczenie stylu z Button */
+const ButtonDelete = styled(Button)`
     color: white;
-    background: #D9534F; /* Kolor tła przycisku Usuń */
+    background: #D9534F;
     border: 1px solid #D9534F;
 
     &:hover {
         background: white;
-        color: #D9534F; /* Kolor tekstu na hover */
-        
+        color: #D9534F;
     }
 `;
 
-const VehicleCard = ({ vehicle, onClick, onDelete, onUpdate }) => {
-    // Sprawdzenie, czy vehicle istnieje, aby uniknąć błędów
-    if (!vehicle) return null; // Zwróć null, jeśli vehicle jest undefined lub null
+const VehicleCard = ({ vehicle, onClick, onDelete, onEdit }) => {
+    if (!vehicle) return null;
 
     return (
-        <Card onClick={onClick}> {/* Dodajemy onClick do Card */}
+        <Card onClick={onClick}>
             <Title>{vehicle.brand} {vehicle.model}</Title>
             <Info>Nr Rejestracyjny: {vehicle.registrationNumber}</Info>
             <div>
                 <ButtonDelete onClick={(e) => { e.stopPropagation(); onDelete(vehicle.id); }}>Usuń</ButtonDelete>
-                <Button onClick={(e) => { e.stopPropagation(); onUpdate(vehicle.id, { /* Zaktualizowane dane */ }); }}>Edytuj</Button>
+                <Button onClick={(e) => { e.stopPropagation(); onEdit(vehicle); }}>Edytuj</Button>
             </div>
         </Card>
     );
